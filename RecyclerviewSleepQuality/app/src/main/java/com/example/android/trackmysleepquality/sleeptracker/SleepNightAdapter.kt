34c8036a9ea3e7a646bf72d2913f7e8cebbbc1cb
FelipePelegrino <felipe.pelegrino99@gmail.com>
2021-08-19 15:35:14 -0300

    Simple RecyclerView with TextView

--- RecyclerviewSleepQuality/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ RecyclerviewSleepQuality/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -15,3 +15,52 @@
  */
 
 package com.example.android.trackmysleepquality.sleeptracker
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.database.SleepNight
+
+/**
+ * Mesma lógica como visto na matéria de DMO em Java
+ * Só se atentar a quando for utilizar mais de 1 layout na view, fazer as devidas tratativas
+ * Com viewHolder e viewType no onCreate
+ */
+
+class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
+
+    /**
+     * Notificar o recyclerView quando o dado for atualizado
+     * Mas usar notifyDataSetChanged() pode ser muito custoso as vezes, pois ele indicará que toda a RecyclerView
+     * terá que ser atualizada, o que em listas mais complexas, pode deixar o app lento
+     * Logo ira ser passado outras formas de atualizar os dados no RecyclerView
+     */
+    var data = listOf<SleepNight>()
+    set(value) {
+        field = value
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount() = data.size
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        val item = data[position]
+        holder.textView.text = item.sleepQuality.toString()
+
+        if (item.sleepQuality <= 1) {
+            holder.textView.setTextColor(Color.RED)
+        } else {
+            holder.textView.setTextColor(Color.BLACK)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
+        return TextItemViewHolder(view)
+    }
+}
